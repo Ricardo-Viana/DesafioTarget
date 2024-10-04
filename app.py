01c70a122379c7eddd2a78067fe7e9cbe77d52ab
Ricardo Viana <ricardo.viana18@gmail.com>
2024-10-04 11:49:38 -0300

    desafio 2

--- app.py
+++ app.py
@@ -11,12 +11,22 @@ def fibonacciSequence(number, penultimate_number, last_number, fibonnaci_value):
 
 print(fibonacciSequence(int(input("Digite um número para saber se ele pertecen a sequencia de fibonnaci: ")), 0, 1, 0))
 
-def inverterCaractere(palavra):
-    palavra_invertida = ""
-    contador_caractere = len(palavra) - 1
-    while(len(palavra_invertida) != len(palavra)):
-        palavra_invertida = palavra_invertida + palavra[contador_caractere]
-        contador_caractere = contador_caractere - 1
-    return palavra_invertida
+def findA(palavra):
+    cont = 0
+    for letra in palavra:
+        if letra.lower() == 'a':
+            cont += 1
+    return cont
 
-print("Palavra invertida: ", inverterCaractere(input("Digite a palavra que você quer inverter: ")))
+print(findA(input("Digite a palavra que queira ver a quantidade de As: ")))
+
+def questao3():
+    indice = 12
+    soma = 0
+    k = 1
+    while k < indice:
+        k = k + 1
+        soma = soma + k
+    return soma
+
+print(questao3())
